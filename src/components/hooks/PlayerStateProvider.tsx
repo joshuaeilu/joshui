46c92684d35522a001467c6d2f5cbb22c2e71e20
brennenputh <brennenputh@gmail.com>
2024-03-05 12:45:41 -0700

    Play the first foreground audio track

--- src/components/hooks/PlayerStateProvider.tsx
+++ src/components/hooks/PlayerStateProvider.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export type PlayerState = {
   wheel: Wheel | null;
   currentStepIdx: number;
-  currentBackgroundAudioIdx: number;
+  currentBgAudioIdx: number;
   background_audio: HTMLAudioElement;
   foreground_audio: HTMLAudioElement;
 }
@@ -13,7 +13,7 @@ export type PlayerState = {
 export const defaultPlayerStateContext = {
   wheel: null,
   currentStepIdx: 0,
-  currentBackgroundAudioIdx: 0,
+  currentBgAudioIdx: 0,
   background_audio: new Audio(),
   foreground_audio: new Audio()
 };
@@ -32,23 +32,28 @@ export function PlayerStateProvider({
 
   function setActiveWheel(wheel: Wheel) {
     const newPlayerState = {...playerState, wheel};
-    const bgAudio = newPlayerState.background_audio;
-    newPlayerState.currentBackgroundAudioIdx = 0;
+    newPlayerState.currentBgAudioIdx = 0;
     newPlayerState.currentStepIdx = 0;
-    bgAudio.src = wheel.background_audio[newPlayerState.currentBackgroundAudioIdx].audio_file;
+
+    const bgAudio = newPlayerState.background_audio;
+    bgAudio.src = wheel.background_audio[newPlayerState.currentBgAudioIdx].audio_file;
     bgAudio.onended = () => {
-      newPlayerState.currentBackgroundAudioIdx += 1;
-      let nextAudio = wheel.background_audio[newPlayerState.currentBackgroundAudioIdx]?.audio_file;
+      newPlayerState.currentBgAudioIdx += 1;
+      let nextAudio = wheel.background_audio[newPlayerState.currentBgAudioIdx]?.audio_file;
       if(nextAudio == null) {
-        newPlayerState.currentBackgroundAudioIdx = 0;
-        nextAudio = wheel.background_audio[newPlayerState.currentBackgroundAudioIdx].audio_file;
+        newPlayerState.currentBgAudioIdx = 0;
+        nextAudio = wheel.background_audio[newPlayerState.currentBgAudioIdx].audio_file;
       }
       bgAudio.src = nextAudio;
-      bgAudio.load();
       bgAudio.play();
     }
-    bgAudio.load();
+
+    const fgAudio = newPlayerState.foreground_audio;
+    fgAudio.src = wheel.steps[0].foreground_audio;
+    
+    fgAudio.play();
     bgAudio.play();
+
     setPlayerState(newPlayerState);
   }
 
