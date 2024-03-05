d0da42ba3fe9f16b168e4ee5a6ba61cd3c13b35d
brennenputh <brennenputh@gmail.com>
2024-03-05 09:10:52 -0700

    Add the actual footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import { IonFooter, IonIcon, IonRange, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <IonFooter>
+      <IonToolbar>
+        <IonTitle>Title of Wheel</IonTitle>
+        <IonRow>
+          
+        </IonRow>
+      </IonToolbar>
+    </IonFooter>
+  )
+}
+
+export default Footer;
