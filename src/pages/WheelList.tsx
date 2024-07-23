5727eb1261890e5f00c9eb989c384964089f8845
Brennen Puth <brennenputh@gmail.com>
2024-07-23 16:18:16 -0400

    Add wheel search in wheels list

--- src/pages/WheelList.tsx
+++ src/pages/WheelList.tsx
@@ -1,4 +1,4 @@
-import { IonButtons, IonContent, IonHeader, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonInput, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import { Wheel } from '../Types';
 import { API_URL } from '../App'
@@ -7,12 +7,17 @@ import { WheelListItem } from '../components/ListItem';
 
 const WheelList: React.FC = () => {
   const [wheels, setWheels] = useState<Wheel[]>([])
+  const [searchTerm, setSearchTerm] = useState("")
 
   useEffect(() => { getWheels() }, [])
 
   const getWheels = async () => {
     const response = await fetch(`${API_URL}/wheels/`)
     setWheels(await response.json())
+  }
+
+  const setFilter = (filter: string) => {
+    setSearchTerm(filter)
   }
 
   return <IonPage>
@@ -25,9 +30,10 @@ const WheelList: React.FC = () => {
       </IonToolbar>
     </IonHeader>
     <IonContent fullscreen={true}>
+      <IonInput className="ion-padding" label="Search" labelPlacement="floating" placeholder="Enter your search term..." onIonInput={(e) => setFilter((e.target as HTMLIonInputElement).value as string)}/>
       {wheels.length != 0 && <IonList lines="none" className="wheellist">
         {
-          wheels.map((data) => {
+          wheels.filter((wheel) => wheel.title.includes(searchTerm) || wheel.description.includes(searchTerm)).map((data) => {
             const totalTime = data.steps.reduce(
               (accumulator, currentValue) => accumulator + currentValue.length, 0
             )
