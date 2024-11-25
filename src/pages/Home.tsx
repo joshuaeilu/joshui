f34a1d7d3510044ecb9d5ab5671dfdb69274c8f9
brennenputh <brennenputh@gmail.com>
2024-11-25 13:20:28 -0500

    Basic implementation of markdown intro and support pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,38 @@
-import { IonButtons, IonCard, IonContent, IonGrid, IonHeader, IonIcon, IonMenuButton, IonPage, IonRouterLink, IonText, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButtons, IonCard, IonContent, IonGrid, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 import { cogOutline, searchOutline } from "ionicons/icons";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
+import { API_URL } from "../App";
+import Markdown from "react-markdown";
 
-const QuickstartCard = ({name, icon, url}: {name: string, icon: string, url: string}) => {
+const QuickstartCard = ({ name, icon, url }: { name: string, icon: string, url: string }) => {
   const history = useHistory()
 
   return <IonCard className="ion-padding" onClick={(e) => {
     e.preventDefault()
     history.push(url)
   }}>
-    <IonIcon icon={icon} style={{ fontSize: 64 }}/>
+    <IonIcon icon={icon} style={{ fontSize: 64 }} />
     <h3>{name}</h3>
   </IonCard>
 }
 
 const Home = () => {
+  const [markdown, setMarkdown] = useState<string | null>();
+
+  const getMarkdown = async () => {
+    const request = await fetch(`${API_URL}/mdpages/intro_page`)
+    if (!request.ok) {
+      return
+    }
+    const data = await request.json()
+    setMarkdown(data)
+  }
+
+  useEffect(() => {
+    getMarkdown()
+  }, [])
+
   return <IonPage>
     <IonHeader>
       <IonToolbar>
@@ -26,10 +44,11 @@ const Home = () => {
     </IonHeader>
     <IonContent>
       <IonGrid className="ion-padding ion-text-center ion-justify-content-center">
-        <h1>Welcome to Prayer Wheels!</h1>
-        <p>Prayer Wheels is a project by Covered Ministries to provide tools for structured and continuous prayer.</p>
-        <p>You can find wheels by either searching the <IonRouterLink routerLink="/wheellist">Find Wheels</IonRouterLink> tab or by getting a link from a friend.</p>
-        <p>This app is currently in <IonText color="danger"><b>alpha</b></IonText>.  Please report any issues through the links in the <IonRouterLink routerLink="/support">Support</IonRouterLink> tab.</p>
+        {markdown ?
+        <Markdown>{markdown}</Markdown>
+        :
+        <h3>Welcome to Prayer Wheels!</h3>}
+
         <h3>Quick Start</h3>
         <QuickstartCard name="Find Wheels" icon={searchOutline} url="/wheellist" />
         <QuickstartCard name="Settings" icon={cogOutline} url="/settings" />
--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,6 +1,22 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import { API_URL } from "../App";
 
 const Support = () => {
+  const [markdown, setMarkdown] = useState<string | null>();
+
+  const getMarkdown = async () => {
+    const request = await fetch(`${API_URL}/mdpages/support_page`)
+    if (!request.ok) return
+    const data = await request.json()
+    setMarkdown(data)
+  }
+
+  useEffect(() => {
+    getMarkdown()
+  }, [])
+
   return <IonPage>
     <IonHeader>
       <IonToolbar>
@@ -11,8 +27,11 @@ const Support = () => {
       </IonToolbar>
     </IonHeader>
     <IonContent>
-      <div style={{ marginLeft: 15 }}>
-        <p>You can reach the Prayer Wheel team at <a href="mailto:prayerwheeladmin@coveredministries.com">prayerwheeladmin@coveredministries.com</a>.</p>
+      <div style={{ marginLeft: 15 }} className="ion-padding ion-text-center">
+        {markdown ?
+        <Markdown>{markdown}</Markdown>
+        :
+        <p>Contacts can be found on the Covered Ministries site.</p>}
       </div>
     </IonContent>
   </IonPage>
