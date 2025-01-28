df1a58c6458a25a46cdb3206286f0b9bdb19ade1
brennenputh <brennenputh@gmail.com>
2025-01-28 18:50:23 -0500

    Fix bottom bar to show times

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,9 +1,19 @@
-import { IonButton, IonCol, IonFooter, IonIcon, IonRange, IonRow, IonToolbar, useIonModal } from '@ionic/react';
+import { IonButton, IonCol, IonFooter, IonIcon, IonRange, IonRow, IonText, IonToolbar, useIonModal } from '@ionic/react';
 import { usePlayerState } from './hooks/PlayerStateProvider';
 import { pause, play, playSkipForward } from 'ionicons/icons';
 import FullscreenPlayer from './modals/FullscreenPlayer';
-import PlayerStepItem from './player/PlayerStepItem';
 import { useTimer } from './hooks/TimerProvider';
+
+const formatTime = (milliseconds: number): string => {
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+};
 
 function PlayerControls() {
   const playerStateContext = usePlayerState();
@@ -34,31 +44,35 @@ function PlayerControls() {
     <div onClick={() => openFullscreen()}>
       <IonToolbar>
         <IonRow style={{
-          height: 55,
+          height: 50,
           overflowY: "hidden",
           overflowX: "hidden",
           flexWrap: "nowrap"
         }}>
-          <IonCol size="auto">
+          <IonCol size="auto" style={{ paddingTop: 0 }}>
             <p style={{
               margin: 0,
               fontSize: "medium"
-            }}><b>{wheel.title}</b></p>
+            }}><b>{wheel?.steps[playerState.curStpIdx].head}</b></p>
             <p style={{
               margin: 0,
               fontSize: "smaller"
-            }}>{wheel?.steps[playerState.curStpIdx].head}</p>
+            }}>{wheel.title}</p>
           </IonCol>
-          <IonCol>
-            {/* TODO: Make a way to get current full wheel progress and use that instead of step progress */}
+          <IonCol style={{ paddingTop: 0 }}>
+            <div style={{ display: "flex", justifyContent: "space-between", fontSize: "smaller", maxHeight: 3 }}>
+              <p style={{fontSize: "smaller"}}>{formatTime(wheel?.steps[playerState.curStpIdx].length - (timerContext.timer.timerSeconds * 1000))}</p>
+              <p style={{fontSize: "smaller"}}>{formatTime(wheel?.steps[playerState.curStpIdx].length)}</p>
+            </div>
             <IonRange
               value={(wheel?.steps[playerState.curStpIdx].length / 1000) - timerContext.timer.timerSeconds}
               min={0}
               max={wheel?.steps[playerState.curStpIdx].length / 1000}
+              disabled
               style={{
                 maxHeight: 16
               }}
-            />
+            ></IonRange>
             <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
               <IonButton onClick={(e) => {
                 e.stopPropagation()
