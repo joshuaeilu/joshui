2a913ea8cafa5bba05972a80671f984b7bfd9985
Josh Laptop <je38@calvin.edu>
2026-03-09 14:14:24 -0600

    Case-insensitivity fix

--- src/App.tsx
+++ src/App.tsx
@@ -102,5 +102,5 @@ const App = () => (
 
 export default App;
 
-export const API_URL =
-  import.meta.env.VITE_API_BASE_URL ?? 'https://api-new.prayerwheel.coveredministries.com'
+export const API_URL = 'http://127.0.0.1:8000'
+  // import.meta.env.VITE_API_BASE_URL ?? 'https://api-new.prayerwheel.coveredministries.com'
